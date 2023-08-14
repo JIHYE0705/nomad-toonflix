9306551ada221b0e2223f1abdd18eedb7c7e1a8b
JIHYE <jihyem75@gmail.com>
2023-08-14 17:54:24 +0900

    :sparkles: feat: add pause

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,6 +12,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
 
   int totalSeconds = 1500;
+  bool isRunning = false;
+
   late Timer timer;
 
   void onTick(Timer timer) {
@@ -25,6 +27,16 @@ class _HomeScreenState extends State<HomeScreen> {
       const Duration(seconds: 1),
       onTick,
     );
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
   }
 
   @override
@@ -52,8 +64,8 @@ class _HomeScreenState extends State<HomeScreen> {
               child: IconButton(
                 iconSize: 120,
                 color: Theme.of(context).cardColor,
-                onPressed: onStartPressed,
-                icon: const Icon(Icons.play_circle_outline),
+                onPressed: isRunning ? onPausePressed : onStartPressed,
+                icon: Icon(isRunning ? Icons.pause_circle_outline : Icons.play_circle_outline),
               ),
             ),
           ),
