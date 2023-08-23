ffc0f1c5942f40b55ade2cafdaa0facf1b05fa89
JIHYE <jihyem75@gmail.com>
2023-08-23 16:45:41 +0900

    :lipstick: design: create appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/toonflix_home_screen.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(),
+      home: Container(
+        child: HomeScreen(),
+      ),
     );
   }
 }
